69604d326585ac38cba98647847a4f12752294f8
jordantangy@gmail.com <jordantangy@gmail.com>
2021-04-21 14:50:21 +0300

    up

--- NumberWithUnits.hpp
+++ NumberWithUnits.hpp
@@ -12,6 +12,7 @@ namespace ariel{
         string unit;
         NumberWithUnits(){}
         NumberWithUnits(double num, string unit):num(num), unit(unit){}
+        
         NumberWithUnits& operator+(const NumberWithUnits& other) const;
         NumberWithUnits& operator+=(const NumberWithUnits& other) const;
         NumberWithUnits& operator+();
@@ -33,35 +34,40 @@ namespace ariel{
             return is;
         }
         static vector<vector<string>> read_units(ifstream& file){
-            string delimiter = "=";
-            vector<vector<string>> v;
-            vector<string> h;
-	        if (!file) {
-		        throw "file not found";
-	        }
-	        else {
-                string s;
-	            while (std::getline(file, s)){
-                size_t pos = 0;
-                string token;
-                    while ((pos = s.find(delimiter)) != std::string::npos) {
-                        token = s.substr(0, pos);
-                        h.push_back(token);
-                        s.erase(0, pos + delimiter.length());
-                    }
-                h.push_back(s);
-                v.push_back(h);
-                h.clear(); 
-            }
-    	}
-            file.close();
-return v;
 
-}
+            if (!file) {
+	            throw "file not found";
+	        }
+            char delimiter = ' ';
+            string s;
+            string h;
+            vector<vector<string>> v;
+            vector<string> vec;
+            while(getline(file,s)){
+                for (size_t i = 0; i < s.size(); i++)
+                {   
+                    if(s.at(i) != delimiter){
+                        h.push_back(s.at(i));
+                        if( i == s.size()-1){
+                            vec.push_back(h);
+                            h.clear();
+                        }
+                    }
+                    else{
+                    vec.push_back(h);
+                    h.clear();
+                    }
+                }
+                v.push_back(vec);
+                vec.clear();
+                h.clear();  
+            }
+            file.close();
+            return v;
+        }
     };
     
     NumberWithUnits& operator*(double num,NumberWithUnits& nwu);
-
 
 }
 
